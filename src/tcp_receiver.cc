d73dcc22eee154e08cd239b4757dbcc2282358a1
schulice <lyxx2413@outlook.com>
2024-04-10 11:23:17 +0800

    finish lab3

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,10 +5,9 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  (void)message;
   // add 1 to get abs seqno
   if ( message.SYN )
-    zero_point_ = message.seqno + 1, initialized_zero_point_ = true;
+    zero_point_ = message.seqno, initialized_zero_point_ = true;
   if ( message.RST )
     reassembler_.reader().set_error();
   if ( !initialized_zero_point_ )
@@ -16,7 +15,7 @@ void TCPReceiver::receive( TCPSenderMessage message )
   const auto insert_index = [&]() -> uint64_t {
     if ( message.SYN )
       return 0;
-    return message.seqno.unwrap( zero_point_, reassembler_.writer().bytes_pushed() );
+    return message.seqno.unwrap( zero_point_ + 1, reassembler_.writer().bytes_pushed() );
   }();
   reassembler_.insert( insert_index, message.payload, message.FIN );
 }
@@ -25,7 +24,7 @@ TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
   const auto ackno = initialized_zero_point_ ? std::optional<Wrap32>(
-                       zero_point_ + reassembler_.writer().bytes_pushed() + reassembler_.writer().is_closed() )
+                       zero_point_ + 1 + reassembler_.writer().bytes_pushed() + reassembler_.writer().is_closed() )
                                              : std::nullopt;
   const auto window_size = reassembler_.writer().available_capacity() > UINT16_MAX
                              ? (uint16_t)UINT16_MAX
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,20 +3,36 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
 
 #include <cstdint>
 #include <functional>
-#include <list>
-#include <memory>
-#include <optional>
-#include <queue>
+#include <map>
+
+class RetransmissionsTimer
+{
+public:
+  RetransmissionsTimer(uint64_t initial_RTO_ms = 0) : RTO_ms_(initial_RTO_ms) {};
+  void reset_RTO(uint64_t initial_RTO_ms); 
+  void pass(uint64_t passed_ms);
+  bool is_activated(); 
+  bool is_closed();
+  void double_RTO();
+  void reset(); 
+  void close();
+private:
+  uint64_t tick_ms_ {};
+  uint64_t RTO_ms_;
+  bool active_ {};
+  bool close_ {};
+};
 
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), timer_( initial_RTO_ms )
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -48,4 +64,14 @@ private:
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  RetransmissionsTimer timer_ {};
+  uint64_t receive_index_ {0};
+  uint64_t send_index_ {};
+  uint64_t window_size_ {1};
+  uint64_t retransmission_time_ {};
+  std::map<uint64_t, TCPSenderMessage> outstanding_seg_ {};
+  bool is_initialized_ {};
+  bool is_closed_ {};
+  void insert_buffer_(uint64_t index, TCPSenderMessage msg);
 };
+
